18c8441d5165630c05439a0eba710ce0f86891db
Maja Glowania <maiaglowania13@gmail.com>
2024-04-21 13:05:49 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-console.log("Cześć świecie!");
+{
+  const welcome = () => {
+    console.log("Cześć świecie!");
+  };
 
-let buttonElement = document.querySelector(".js-section__button");
-let photoElement = document.querySelector(".js-section__photo");
-let buttonTextElement = document.querySelector(".js-buttonText");
+  const hidePhoto = () => {
+    const photoElement = document.querySelector(".js-section__photo");
+    const buttonTextElement = document.querySelector(".js-buttonText");
 
-buttonElement.addEventListener("click", () => {
-  photoElement.classList.toggle("js-noSection__photo");
-  buttonTextElement.classList.toggle("pokaż");
+    photoElement.classList.toggle("js-noSection__photo");
+    buttonTextElement.classList.toggle("pokaż");
 
-  buttonTextElement.innerText = photoElement.classList.contains(
-    "js-noSection__photo"
-  )
-    ? "pokaż"
-    : "ukryj";
-});
+    buttonTextElement.innerText = photoElement.classList.contains(
+      "js-noSection__photo"
+    )
+      ? "pokaż"
+      : "ukryj";
+  };
+
+  const init = () => {
+    const buttonElement = document.querySelector(".js-section__button");
+    buttonElement.addEventListener("click", hidePhoto);
+
+    welcome();
+  };
+
+  init();
+}
